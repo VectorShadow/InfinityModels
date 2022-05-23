e654052ea08199a97e79c10b13db4af720f83169
vector_shadow <vectorshadowdigitallabs@gmail.com>
2022-05-23 17:32:38 -0500

    Added turn length constant

--- src/main/java/org/vsdl/astral/models/util/Constants.java
+++ src/main/java/org/vsdl/astral/models/util/Constants.java
@@ -1,5 +1,8 @@
 package org.vsdl.astral.models.util;
 
 public class Constants {
+    //engine
+    public static final int TURN_LENGTH_MS = 125;
+    //world
     public static final int ZONE_SIZE = 64;
 }
